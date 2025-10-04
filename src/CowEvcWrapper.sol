c2adf5b5c7816522af62b06b845cb330d61e3614
Kaze <kaze@cow.fi>
2025-10-04 15:12:56 +0900

    feat: experiment with using borrower for euler
    please do not review this deeply as its just a branch off experiment to ensure we have a picture of what a potential borrower implementation would look like.
    
    overall doesn't seem like a great approach becuase:
    * determining the "borrower" of the funds and/or making sure it is securely set is a major challenge.
    triggerFlashLoan only gives us lender, token, and amount. token can probably considered MUST be the the actual token being borrowed (due to the design of flashloans/approve borrower function). There may be some wiggle room here as technically the Borrower code can be modified for euler, but its unknow nwhat off-chain impacts this could have. amount is needed whether we are doing Aave or Euler. So that leaves lender, which is intended to be the contract that issues the loan, and we would really like this to be the actual EVault contract. If we use "lender" as the user/beneficiary of the trade instead, then we have to have some way to lookup the EVault contract associated with a particular token (and because of how Euler's community structure works, their may be many). The other options to solve this are
    use a prehook
    * on repay this data will have to be signed by the user because th erepay beneficiary could be malliable by the solver
    use extra calldata from callBackData, the final argument
    * this would require for the euler code to extract the data from the field and return th ecallback with that data removed. I am almos tcertain it would require solver/driver modifications to make this work properly
    * im pretty sure this has the same issue as the prehook method, the data must be signed when repaying, if the borrower is somehow allowed to be set incorrectly
    certain operations of the borrow/repay functions require authenticated calls to execute. for example, to borrow, you need to call the authenticated enableCollateral and enableController on the EVC to permit creation of the loan. in our current testing implementation, there is a temporary authorization granted by IEVC.permit https://github.com/cowprotocol/euler-integration-contracts/blob/feat/initial/test/CowEvcWrapperTest.t.sol#L190 , but its unclear how this permit could be easily replicated with flashloan router. The EVC does support another function, setOperator https://evc.wtf/docs/contracts/technical-reference/contract.EthereumVaultConnector#setaccountoperator, which would allow for the user to specify in advance . But then that gives the EVC contract basically unchecked governing authority over the user's loan, funds, and collateral transfers, which would likely be a security issue (not an issues for the permit because the permitted EVC calls are signed and only for the one purpose the user calls for)
    * the collateral that the user wants to borrow will need to be enabled prior to lending by calling enableCollateral, since the flashloan wrapper doesn't really know what the borrowed collateral is from its params.
    * this is less of a "problem" per se, but the flashloan router was clearly not designed with Euler's "non flashloan" flow in mind. For example, on repay its going to be a "flashloan" call where no loan is actually taken, the funds to repay are sent from the settlement contract to the flashloan router (most likely), and then the flashloan router calls "repay" with the "amount" to flashloan. even though we dont see it now, we are probably going to see more issues like the above appear just because we are using th eflashloan router for something it fundamentally doesn't do...

--- src/CowEvcWrapper.sol
+++ src/CowEvcWrapper.sol
@@ -5,17 +5,18 @@ import {IEVC} from "evc/EthereumVaultConnector.sol";
 //import {IGPv2Settlement, GPv2Interaction} from "./vendor/interfaces/IGPv2Settlement.sol";
 import {IGPv2Authentication} from "./vendor/interfaces/IGPv2Authentication.sol";
 
-import {GPv2Signing, IERC20, GPv2Trade, GPv2Order} from "cow/mixins/GPv2Signing.sol";
-import {GPv2Wrapper,GPv2Interaction} from "cow/GPv2Wrapper.sol";
+import {GPv2Signing, GPv2Trade, GPv2Order} from "cow/mixins/GPv2Signing.sol";
+import {GPv2Interaction} from "cow/libraries/GPv2Interaction.sol";
 import {IERC4626, IBorrowing} from "euler-vault-kit/src/EVault/IEVault.sol";
+
+import {Borrower, IERC20, IFlashLoanRouter} from "flash-loan-router/mixin/Borrower.sol";
 
 /// @title CowEvcWrapper
 /// @notice A wrapper around the EVC that allows for settlement operations
-contract CowEvcWrapper is GPv2Wrapper, GPv2Signing {
+contract CowEvcWrapper is Borrower, GPv2Signing {
     IEVC public immutable EVC;
 
-    /// @notice 0 = not executing, 1 = wrappedSettle() called and not yet internal settle, 2 = evcInternalSettle() called
-    uint256 public transient settleState;
+    address public transient borrower;
 
     mapping(bytes32 => IEVC.BatchItem[]) private postActions;
 
@@ -27,7 +28,7 @@ contract CowEvcWrapper is GPv2Wrapper, GPv2Signing {
 
     error NotEVCSettlement();
 
-    constructor(address _evc, address payable _settlement) GPv2Wrapper(_settlement) {
+    constructor(address _evc, IFlashLoanRouter _flashLoanRouter) Borrower(_flashLoanRouter) {
         EVC = IEVC(_evc);
     }
 
@@ -37,123 +38,55 @@ contract CowEvcWrapper is GPv2Wrapper, GPv2Signing {
         uint256 minAmount;
     }
 
-    function setRequiredPostActions(
-        bytes32 orderDigest,
-        IEVC.BatchItem[] calldata actions
-    ) external {
-        postActions[orderDigest] = actions;
+    // TODO: need to set up some sort of signing/permit here because this is malleable by solver
+    function setBorrower(address _borrower) external {
+        borrower = _borrower;
     }
 
-    function executePostActions(
-        bytes32 orderDigest
-    ) external {
-        if (postActions[orderDigest].length > 0) {
-            EVC.batch(postActions[orderDigest]);
-            postActions[orderDigest] = new IEVC.BatchItem[](0);
-        }
+    /// @notice Implementation of Borrower.triggerFlashLoan, which allows for a euler position to be opened
+    /// PRIOR to calling this function, `setOperator` must be called to allow this function to execute on behalf of the `borrower` set in `setBorrower`.
+    function triggerFlashLoan(address lender, IERC20 token, uint256 amount, bytes calldata callBackData)
+        internal
+        override
+    {
+        IEVC.BatchItem[] memory items = new IEVC.BatchItem[](3);
 
-    }
+        // this call needs to happen before calling this function because we dont know what the borrowed collateral is
+        //items[0] = IEVC.BatchItem({
+        //    onBehalfOfAccount: address(0),
+        //    targetContract: address(evc),
+        //    value: 0,
+        //    data: abi.encodeCall(IEVC.enableCollateral, (borrower, ???))
+        //});
+        //items[0] = IEVC.BatchItem({
+        //    onBehalfOfAccount: address(0),
+        //    targetContract: address(EVC),
+        //    value: 0,
+        //    data: abi.encodeCall(IEVC.enableController, (borrower, lender))
+        //});
 
-    // helper function to execute repay
-    function helperRepayAndReturn(
-        address vault,
-        address beneficiary,
-        uint256 maxRepay
-    ) external {
-        IERC20 asset = IERC20(IERC4626(vault).asset());
-        asset.approve(vault, type(uint256).max);
-        IBorrowing(vault).repay(maxRepay, beneficiary);
-
-        // transfer any remaining dust back to the user
-        asset.transfer(beneficiary, asset.balanceOf(address(this)));
-    }
-
-    /// @notice Implementation of GPv2Wrapper._wrap - executes EVC operations around settlement
-    /// @param tokens Tokens involved in settlement
-    /// @param clearingPrices Clearing prices for settlement
-    /// @param trades Trade data for settlement
-    /// @param interactions Interaction data for settlement
-    /// @param wrapperData Additional data for the wrapper (unused in this implementation)
-    function _wrap(
-        IERC20[] calldata tokens,
-        uint256[] calldata clearingPrices,
-        GPv2Trade.Data[] calldata trades,
-        GPv2Interaction.Data[][3] calldata interactions,
-        bytes calldata wrapperData
-    ) internal override {
-        // prevent reentrancy: there is no reason why we would want to allow it here
-        if (settleState != 0) {
-            revert NoReentrancy();
-        }
-        settleState = 1;
-
-        // Decode wrapperData into pre and post settlement actions
-        (IEVC.BatchItem[] memory preSettlementItems, IEVC.BatchItem[] memory postSettlementItems) =
-            abi.decode(wrapperData, (IEVC.BatchItem[], IEVC.BatchItem[]));
-        IEVC.BatchItem[] memory items = new IEVC.BatchItem[](preSettlementItems.length + postSettlementItems.length + 1 + trades.length);
-
-        // Copy pre-settlement items
-        for (uint256 i = 0; i < preSettlementItems.length; i++) {
-            items[i] = preSettlementItems[i];
-        }
-
-        // Add settlement call to wrapper - use _internalSettle from GPv2Wrapper
-        items[preSettlementItems.length] = IEVC.BatchItem({
-            onBehalfOfAccount: msg.sender,
-            targetContract: address(this),
+        // the below call requires operator permissions in order to borrow on behalf of account
+        items[1] = IEVC.BatchItem({
+            onBehalfOfAccount: borrower,
+            targetContract: address(lender),
             value: 0,
-            data: abi.encodeCall(this.evcInternalSettle, (tokens, clearingPrices, trades, interactions))
+            data: abi.encodeCall(IBorrowing.borrow, (amount, address(settlementContract)))
         });
 
-        // Copy post-settlement items
-        for (uint256 i = 0; i < postSettlementItems.length; i++) {
-            items[preSettlementItems.length + 1 + i] = postSettlementItems[i];
-        }
-
-        // Users can force post settlement actions to also occur in `preSettlementItems`. So we call ourself to enforce this
-        GPv2Order.Data memory o;
-        for (uint256 i = 0;i < trades.length;i++) {
-            GPv2Trade.extractOrder(trades[i], tokens, o);
-            bytes32 orderDigest = GPv2Order.hash(
-                o,
-                UPSTREAM_SETTLEMENT.domainSeparator() // TODO can be more efficient
-            );
-            items[preSettlementItems.length + postSettlementItems.length + 1 + i] = IEVC.BatchItem({
-                onBehalfOfAccount: address(this),
-                targetContract: address(this),
-                value: 0,
-                data: abi.encodeCall(this.executePostActions, (orderDigest))
-            });
-        }
+        // this will call settle
+        // the settlement shuold be borrowed token -> euler vault token, into the user's account
+        items[2] = IEVC.BatchItem({
+            onBehalfOfAccount: address(this),
+            targetContract: address(this),
+            value: 0,
+            data: abi.encodeCall(this.callback, (callBackData))
+        });
 
         // Execute all items in a single batch
         EVC.batch(items);
-        settleState = 0;
     }
 
-    /// @notice Internal settlement function called by EVC
-    /// @param tokens Tokens involved in settlement
-    /// @param clearingPrices Clearing prices for settlement
-    /// @param trades Trade data for settlement
-    /// @param interactions Interaction data for settlement
-    function evcInternalSettle(
-        IERC20[] calldata tokens,
-        uint256[] calldata clearingPrices,
-        GPv2Trade.Data[] calldata trades,
-        GPv2Interaction.Data[][3] calldata interactions
-    ) external payable {
-        if (msg.sender != address(EVC)) {
-            revert Unauthorized(msg.sender);
-        }
-
-        if (settleState != 1) {
-            // origSender will be address(0) here which indiates that internal settle was called when it shouldn't be (outside of wrappedSettle call)
-            revert Unauthorized(address(0));
-        }
-
-        settleState = 2;
-
-        // Use GPv2Wrapper's _internalSettle to call the settlement contract
-        _internalSettle(tokens, clearingPrices, trades, interactions);
+    function callback(bytes calldata callBackData) external {
+        flashLoanCallBack(callBackData);
     }
 }
