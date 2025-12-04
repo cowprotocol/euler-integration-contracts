772a202c51936e64a7f4c152805e52f72c33f0e0
Kaze <kaze@cow.fi>
2025-12-04 16:36:40 +0900

    Implement validateWrapperData for open position wrapper
    - Add validateWrapperData implementation to CowEvcOpenPositionWrapper
    - Remove deprecated parseWrapperData function
    - Update tests to use validateWrapperData
    - All tests passing

--- test/unit/CowEvcOpenPositionWrapper.unit.t.sol
+++ test/unit/CowEvcOpenPositionWrapper.unit.t.sol
@@ -143,27 +143,13 @@ contract CowEvcOpenPositionWrapperUnitTest is Test {
                     PARSE WRAPPER DATA TESTS
     //////////////////////////////////////////////////////////////*/
 
-    function test_ParseWrapperData_EmptySignature() public view {
+    function test_ValidateWrapperData_EmptySignature() public view {
         CowEvcOpenPositionWrapper.OpenPositionParams memory params = _getDefaultParams();
 
         bytes memory wrapperData = abi.encode(params, new bytes(0));
-        bytes memory remaining = wrapper.parseWrapperData(wrapperData);
 
-        assertEq(remaining.length, 0, "Should have no remaining data");
-    }
-
-    function test_ParseWrapperData_WithExtraData() public view {
-        CowEvcOpenPositionWrapper.OpenPositionParams memory params = _getDefaultParams();
-
-        bytes memory signature = new bytes(0);
-        bytes memory wrapperData = abi.encode(params, signature);
-        bytes memory extraData = hex"deadbeef";
-        wrapperData = abi.encodePacked(wrapperData, extraData);
-
-        bytes memory remaining = wrapper.parseWrapperData(wrapperData);
-
-        assertEq(remaining.length, 4, "Should have 4 bytes remaining");
-        assertEq(remaining, extraData, "Extra data should match");
+        // Should not revert for valid wrapper data
+        wrapper.validateWrapperData(wrapperData);
     }
 
     /*//////////////////////////////////////////////////////////////
